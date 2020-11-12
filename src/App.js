445cfd2c5fd180fd0e0688f3b961be2aac919e89
GoldCastillo <mikko.linna@myy.haaga-helia.fi>
2020-11-12 10:24:36 +0200

    kyselypalvelu-frontend-sprint1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [questionnaire, setQuestionnaire] = useState({})
+  const [questionnaire, setQuestionnaire] = useState({});
   
   useEffect(() => {
-    fetch('http://localhost:8080/questionnaires/1')
+    fetch("https://kyselypalvelu-backend.herokuapp.com/questionnaires/1")
       .then((response) => response.json())
-      .then((data) => setQuestionnaire(data))
-  }, [])
+      .then((data) => setQuestionnaire(data));
+  }, []);
 
-  if (!questionnaire.questions) return <p>loading...</p>
+  if (!questionnaire.questions) return <p>loading...</p>;
 
+  const onSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      console.log(data.getAll("answer"))
+      const answers = data.getAll("answer");
+      
+
+  };
   return (
     <div>
       <h1>Kyselypalvelu</h1>
       <h2>{questionnaire.title}</h2>
       <p>{questionnaire.description}</p>
 
-      {questionnaire.questions.map((question) => (
-        <div key={question.questionId}>
-          <p>{question.questionText}</p>
-          <input type="text" />
-        </div>
-      ))}
+      <form onSubmit={onSubmit}>
+        {questionnaire.questions.map((question) => (
+          <div key={question.questionId}>
+            <p>{question.questionText}</p>
+            <input type="text" name="answer" />
+          </div>
+        ))} 
+        <input type="submit" value="Lähetä"/>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
